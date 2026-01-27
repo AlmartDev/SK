8eb6463a16ef85b49cf7e9584ffa3185ecfba3ce
almartdev <alonsomartinezcarratala@gmail.com>
2026-01-27 19:48:38 +0100

    Removed: Prototype tests already implemented in the Interpreter

--- python-prototype/examples/test-exponentiation.py
+++
@@ -1,6 +0,0 @@
-from sk import *
-
-print(Sknown(0) ** Sknown(0))        # 1
-print(Sknown(0) ** Sknown(5))        # 0
-print(Sknown(2) ** Sknown(3))        # 8
-print(Sinterval(2,4) ** Sknown(2))   # [4..16]
